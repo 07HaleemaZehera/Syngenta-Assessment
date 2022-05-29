172b9f27ce0b1b5a1aef86ab209db39f73730a21
07HaleemaZehera <hzehera07@gmail.com>
2022-05-29 14:26:03 +0530

    removed the wrong data

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 import "./Styles/HomePage.css";
-import GenerateRandomColor from "./Usecolorgenerate";
+
 
 export default function HomePage() {
-  const { color, generateColor } = GenerateRandomColor();
 
   return (
     <div className="main-container">
       <div className="left-div"></div>
       <div className="circles">
-          <div>"#"{color}</div>
-          <div>"#"{color}</div>
-          <div>"#"{color}</div>
-          <div>"#"{color}</div>
-          <div>"#"{color}</div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
       </div>
       <div className="left-div-container">
           <input className="btn" type="number" placeholder="Enter number" />
-          <button className="btn" onClick={generateColor} >Shoot</button>
+          <button className="btn" onClick >Shoot</button>
       </div>
     </div>
 
--- src/Components/Usecolorgenerate.js
+++ src/Components/Usecolorgenerate.js
@@ -3,10 +3,7 @@ import {useState} from 'react';
   
 const GenerateRandomColor = () => {
     const [color,setColor] = useState("")
-    const generateColor = () =>{
-        setColor(Math.random().toString(16).substr(-6));
-    };
-    return {color,generateColor};
+  
       
 };
 export default GenerateRandomColor;
