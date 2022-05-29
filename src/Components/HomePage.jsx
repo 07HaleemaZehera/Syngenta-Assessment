ad44278ca70e6e3274ef041e9ae0a8876e616d61
07HaleemaZehera <hzehera07@gmail.com>
2022-05-29 17:01:24 +0530

    Done with shoot btn functionality

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -5,9 +5,17 @@ import { AllColorsData } from "./AllColors";
 export default function HomePage() {
   const[color,setColor]=useState(AllColorsData)
 
+  const handleShoot=(id)=>{
+    const colorChange=color.map((e)=>{
+      return e._id==id ? {...e,status:!e.status} : e
+    })
+    setColor(colorChange)
+    console.log("ShowData",AllColorsData)
+  }
+
   return (
     <div className="main-container">
-      <div className="left-div"></div>
+      <div className="left-div">Empty Div</div>
       <div className="circles">
           <div></div>
           <div></div>
@@ -17,7 +25,7 @@ export default function HomePage() {
       </div>
       <div className="left-div-container">
           <input className="btn" type="number" placeholder="Enter number" />
-          <button className="btn" onClick >Shoot</button>
+          <button className="btn" onClick={()=>handleShoot()} >Shoot</button>
       </div>
     </div>
 
